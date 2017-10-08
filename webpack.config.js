9b32777b35414256eb9da6247e9e8c3b9c60d2f3
Julio Olivera <julio.olvr@gmail.com>
2017-10-08 12:08:24 -0700

    Use webpack-dev-server with auto reloading

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new CopyWebpackPlugin([{ from: 'src/lib/phaser.js' }]),
